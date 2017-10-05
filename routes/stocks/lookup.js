c1dc687f672576aeab60f3ec38d08c84201c146c
Kai Chen <kaichenusesthis@gmail.com>
2017-10-05 06:00:35 +0000

    Data now shows in graph

--- routes/stocks/lookup.js
+++ routes/stocks/lookup.js
@@ -1,5 +1,11 @@
 var request = require('request');
 var results = {};
+
+// TODO: put this function elsewhere
+function convertToInt(string) {
+    return parseInt(string)
+}
+
 module.exports = {
     
     lookup: function(req, res) {
@@ -9,9 +15,12 @@ module.exports = {
 
     request('https://www.alphavantage.co/query?function=SMA&symbol=' + ticker + '&interval=daily&time_period=10&series_type=open&apikey=365B4T6IUY7YO4D7', function (error, response, body) {
         var rawReturned = JSON.parse(body);
-        
+        // TODO: Add loop here so it returns data from multiple dates
         var result = rawReturned["Technical Analysis: SMA"]["2017-10-04"].SMA;
-        results.body = JSON.stringify(result);
+        var resultInt = convertToInt(result);
+        
+        
+        results.body = JSON.stringify(resultInt);
         console.log(results.body);
 
     });
