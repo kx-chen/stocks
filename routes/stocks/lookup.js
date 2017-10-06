eac71bdf09f2cf5f3cb74e4a357bb1c3c501ee57
Kai Chen <kaichenusesthis@gmail.com>
2017-10-06 22:03:17 +0000

    All data results now sent to the view

--- routes/stocks/lookup.js
+++ routes/stocks/lookup.js
@@ -1,31 +1,40 @@
 var request = require('request');
-var results = {};
+var results = [];
 
 // TODO: put this function elsewhere
-function convertToInt(string) {
-    return parseFloat(string)
-}
+// function convertToInt(string) {
+//     return parseFloat(string)
+// }
+function parseJson(rawReturned, res, ticker) {
+    var keys = Object.keys(rawReturned["Technical Analysis: SMA"]);
+    
+         for (var i = 0; i < Object.keys(rawReturned["Technical Analysis: SMA"]).length; i++){
 
+            results.push(rawReturned["Technical Analysis: SMA"][keys[i]].SMA);
+            
+            if (i == Object.keys(rawReturned["Technical Analysis: SMA"]).length) {
+                console.log(results)
+                return results
+             }
+        
+        }
+      
+}
+    
 module.exports = {
     
+   
     lookup: function(req, res) {
-        console.log("death is inevitable");
+
         var ticker = req.params.ticker;
-        
 
     request('https://www.alphavantage.co/query?function=SMA&symbol=' + ticker + '&interval=15min&time_period=10&series_type=close&apikey=365B4T6IUY7YO4D7&outputsize=compact', function (error, response, body) {
         var rawReturned = JSON.parse(body);
-        // TODO: Add loop here so it returns data from multiple dates
-        var result = rawReturned[1][0].SMA;
-        var resultInt = convertToInt(result);
+        parseJson(rawReturned, res, ticker);
+        res.render("stocks/lookup.html", {title:"Lookup", ticker: ticker, results: results});
         
-        results.body = JSON.stringify(resultInt);
-        console.log(results.body);
-        
-        res.render("stocks/lookup.html", {title:"Lookup", ticker: ticker, results: results.body });
-
     });
     
-        
+
     }
 };
