76f15fa4abeaa52868fcb6d40b5555c1fa1f18c4
Kai Chen <kaichenusesthis@gmail.com>
2017-10-06 23:12:58 +0000

    line graph now displays stock data

--- routes/stocks/lookup.js
+++ routes/stocks/lookup.js
@@ -12,9 +12,12 @@ function parseJson(rawReturned, res, ticker) {
 
             results.push(rawReturned["Technical Analysis: SMA"][keys[i]].SMA);
             
-            if (i == Object.keys(rawReturned["Technical Analysis: SMA"]).length) {
-                console.log(results)
-                return results
+            console.log(Object.keys(rawReturned["Technical Analysis: SMA"]).length);
+            console.log("==" + i);
+            
+            if (i == Object.keys(rawReturned["Technical Analysis: SMA"]).length - 1) {
+                console.log("hey");
+                res.render("stocks/lookup.html", {title:"Lookup", ticker: ticker, results: results});
              }
         
         }
@@ -28,13 +31,12 @@ module.exports = {
 
         var ticker = req.params.ticker;
 
-    request('https://www.alphavantage.co/query?function=SMA&symbol=' + ticker + '&interval=15min&time_period=10&series_type=close&apikey=365B4T6IUY7YO4D7&outputsize=compact', function (error, response, body) {
+    request('https://www.alphavantage.co/query?function=SMA&symbol=' + ticker + '&interval=daily&time_period=10&series_type=close&apikey=365B4T6IUY7YO4D7&outputsize=compact', function (error, response, body) {
         var rawReturned = JSON.parse(body);
         parseJson(rawReturned, res, ticker);
-        res.render("stocks/lookup.html", {title:"Lookup", ticker: ticker, results: results});
-        
     });
-    
-
+        
+        
+        results = [];
     }
 };
