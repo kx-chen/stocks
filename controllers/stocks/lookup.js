30a8b2d9cc2865f17347c070d10c819797d9a979
Kai <kaichenusesthis@gmail.com>
2017-10-11 14:56:22 -0700

    Data is now showed in chart

--- controllers/stocks/lookup.js
+++ controllers/stocks/lookup.js
@@ -1,31 +1,32 @@
 var request = require('request');
 
-function lookUp(ticker, req, res) {
-    request("https://www.quandl.com/api/v3/datasets/WIKI/" + ticker + "/data.json?api_key=VLTxWTE1vKYjmxBWTiF3&rows=100", function(error, response, body){
+function lookUp(data, req, res) {
+    request("https://www.quandl.com/api/v3/datasets/WIKI/" + data + "/data.json?api_key=VLTxWTE1vKYjmxBWTiF3&rows=100", function(error, response, body){
         
         body = JSON.parse(body);
         var results = body["dataset_data"].data;
 
         var final = parseResults(results);
 
-        var final = results;
-
-        final = '["' + final.join('", "') + '"]';
         console.log(final);
-        res.render("stocks/lookup.html", {"ticker": ticker, "results": final, "title": "Lookup"});
+
+        // final = '["' + final.join('", "') + '"]';
+        // console.log(final);
+        res.render("stocks/lookup.html", {"ticker": data, "results": final, "title": "Lookup"});
     });
     
 }
 
 function parseResults(results) {
-    let parsedData = [];
+    let parsedData = "[";
     results.forEach(function(value, index){
         value = value.slice(0,5);
-        parsedData.push(value);
+        console.log(JSON.stringify(value));
+        parsedData += JSON.stringify(value) + ",";
     });
 
     // console.log(parsedData);
-    return parsedData
+    return parsedData += "]"
 }
 
     
