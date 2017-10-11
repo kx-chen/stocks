f6d461c582eaf41e199dea59fdbbc06a1118a979
Kai <kaichenusesthis@gmail.com>
2017-10-11 16:48:45 -0700

    Redirects to error page if invalid stock entered

--- controllers/stocks/lookup.js
+++ controllers/stocks/lookup.js
@@ -4,24 +4,25 @@ function lookUp(data, req, res) {
     request("https://www.quandl.com/api/v3/datasets/WIKI/" + data + "/data.json?api_key=VLTxWTE1vKYjmxBWTiF3&rows=100", function(error, response, body){
         
         body = JSON.parse(body);
-        var results = body["dataset_data"].data;
 
-        var final = parseResults(results);
-
-        console.log(final);
-
-        // final = '["' + final.join('", "') + '"]';
-        // console.log(final);
-        res.render("stocks/lookup.html", {"ticker": data, "results": final, "title": "Lookup"});
+        if (body["quandl_error"]) {
+            res.render("public/error.html", {title: "Error looking up", message: body.quandl_error.message})
+        } else {
+            var results = body["dataset_data"].data;
+            var final = parseResults(results);     
+            res.render("stocks/lookup.html", {"ticker": data.toUpperCase(), "results": final, "title": "Lookup"});
+        }
+        
     });
     
 }
 
 function parseResults(results) {
     let parsedData = "[";
+
     results.forEach(function(value, index){
         value = value.slice(0,5);
-        console.log(JSON.stringify(value));
+        // console.log(JSON.stringify(value));
         parsedData += JSON.stringify(value) + ",";
     });
 
